f7b973a03ca6532a1766d6999ea89f40bb9a948a
Barlog <barlog@tanelorn.li>
2017-10-15 21:34:19 +0300

    1. Disable console ansi colors 2. Enable command line args log

--- src/main/kotlin/app/App.kt
+++ src/main/kotlin/app/App.kt
@@ -1,8 +1,13 @@
 package app
 
 import app.service.Server
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
 
 fun main(vararg args: String) {
+	logger.info { "args: $args" }
+
 	Runtime.getRuntime().addShutdownHook(Thread(Runnable { Server.stop() }))
 	Server.start(port(*args))
 }
